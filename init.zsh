9f15443297a608f12ab3e6d6bcd83526222e2e11
Emily Marigold Klassen <forivall@gmail.com>
2017-06-29 11:24:07 -0700

    fix: allow using "node-module-template" as the name in package.json

--- init.zsh
+++ init.zsh
@@ -1,2 +1,4 @@
 #!/usr/bin/env zsh
-sed -i -e 's/$NAME/'"${PWD##*/}"'/g' -e 's/$DESCRIPTION/'"$1"'/g' ./**/*(.)
+NAME="${PWD##*/}"
+sed -i -e 's/node-module-template/$NAME/g' ./package.json
+sed -i -e 's/$NAME/'"$NAME"'/g' -e 's/$DESCRIPTION/'"$1"'/g' ./**/*(.)
